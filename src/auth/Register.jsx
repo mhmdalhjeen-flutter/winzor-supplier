53353a5486da87d33df1cff2e595921b19304e59
our_GitHub_user_name <our_GitHub_user_email>
2026-07-07 09:45:18 +0200

    ffff

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import AuthLayout from "../layouts/AuthLayout";
 import "../styles/auth.css";
 import { register } from "../services/auth.service";
